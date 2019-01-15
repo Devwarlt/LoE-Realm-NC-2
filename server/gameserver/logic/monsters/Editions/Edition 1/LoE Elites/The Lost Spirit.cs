94cff1b9f8f86ca9a8878d5cba2c0070e98a00d7
Filisha <30576431+Filisha@users.noreply.github.com>
2019-01-14 21:12:59 -0500

    :fire: hotfix (minor)
    -updated undertaker sprite
    -fixed char slot pricing

--- server/gameserver/logic/monsters/Editions/Edition 1/LoE Elites/The Lost Spirit.cs
+++ server/gameserver/logic/monsters/Editions/Edition 1/LoE Elites/The Lost Spirit.cs
@@ -17,6 +17,7 @@ namespace LoESoft.GameServer.logic
 						),
 					new State("Awakening",
 						new AddCond(ConditionEffectIndex.Invincible),
+						new Taunt("Who was awoken me from my slumber..?"),
 						new Flashing(0xFFFFFF, 2, 3),
 						new TimedTransition(4768, "backandforth")
 						),
@@ -29,7 +30,7 @@ namespace LoESoft.GameServer.logic
 						new Taunt(1.00, "There is no mercy here.", "Prepare to meet your doom!"),
 					    new RemCond(ConditionEffectIndex.Invincible),
 						new Prioritize(
-							 new Chase(0.34, 8, 1),
+							 new Chase(34, 8, 1),
 							 new StayAbove(0.3, 5)
 							),
 						new Shoot(10, shoots: 12, index: 0, coolDown: 3000),
@@ -39,7 +40,7 @@ namespace LoESoft.GameServer.logic
 						),
 					 new State("1",
 						new Grenade(5, 100, range: 6, coolDown: 2000),
-						new ReturnToSpawn(once: false, speed: 0.4),
+						new ReturnToSpawn(once: false, speed: 4),
 						new Shoot(10, shoots: 8, shootAngle: 28, index: 6, coolDown: 2600),
 						new Shoot(10, shoots: 2, shootAngle: 36, index: 0, coolDown: 3000),
 						new Shoot(10, shoots: 8, shootAngle: 60, index: 2, coolDown: 2000),
@@ -59,7 +60,7 @@ namespace LoESoft.GameServer.logic
 						new SetAltTexture(0),
 						new Taunt(0.80, "I shall rip you apart.", "Your life has come to a end."),
 						new Prioritize(
-							 new Chase(0.34, 8, 1),
+							 new Chase(34, 8, 1),
 							 new StayAbove(0.3, 5)
 							),
 						new Shoot(10, shoots: 12, index: 0, coolDown: 3000),
@@ -69,7 +70,7 @@ namespace LoESoft.GameServer.logic
 						new TimedTransition(3250, "2")
 						),
 					 new State("2",
-						new ReturnToSpawn(once: false, speed: 0.4),
+						new ReturnToSpawn(once: false, speed: 4),
 						new Shoot(10, shoots: 2, shootAngle: 36, index: 0, coolDown: 3000),
 						new Shoot(10, shoots: 8, shootAngle: 60, index: 2, coolDown: 2000),
 						new Shoot(10, shoots: 2, index: 1, coolDown: 1500),
@@ -79,13 +80,13 @@ namespace LoESoft.GameServer.logic
 						 )
 						),
 					new State("returntospawn",
-						new ReturnToSpawn(once: true, speed: 0.5),
+						new ReturnToSpawn(once: true, speed: 5),
 						new Shoot(10, shoots: 4, shootAngle: 36, index: 3, coolDown: 3000),
 						new TimedTransition(2000, "invisibleattack")
 						),
 					  new State("invisibleattack",
 					   new Shoot(10, shoots: 8, shootAngle: 28, index: 4, coolDown: 2600),
-					   new Wander(0.1),
+					   new Wander(1),
 					   new AddCond(ConditionEffectIndex.Invincible),
 					   new Shoot(10, shoots: 2, shootAngle: 36, index: 0, coolDown: 3000),
 						new Shoot(10, shoots: 8, shootAngle: 60, index: 2, coolDown: 2000),
@@ -110,10 +111,9 @@ namespace LoESoft.GameServer.logic
 			.Init("SPIRIT Loot Chest",
 			new State(
 		  new State("timed",
-		  new AddCond(ConditionEffectIndex.Invulnerable),
 		  new Taunt("Loot Chest will be available in 5 seconds."),
-		  new TimedTransition(5000, "loot"),
-		  new RemCond(ConditionEffectIndex.Invincible)
+		  new AddCond(ConditionEffectIndex.Invulnerable, 5000),
+		  new TimedTransition(5000, "loot")
 			  ),
 		  new State("loot"
 			  )),
@@ -121,10 +121,10 @@ namespace LoESoft.GameServer.logic
 					new ItemLoot("Potion of Defense", 1),
 					new ItemLoot("Potion of Defense", 0.5),
 					//LT
-					new ItemLoot("Crossbow of the Frozen North", 0.01),
-					new ItemLoot("Phantom Light-X", 0.01),
+					new ItemLoot("Crossbow of the Frozen North", 0.05),
+					new ItemLoot("Phantom Light-X", 0.05),
 					//SRT
-					new ItemLoot("Galatic Axe", 0.0094)
+					new ItemLoot("Galatic Axe", 0.001)
 					 ),
 				 new MostDamagers(10,
 					LootTemplates.StatIncreasePotionsLoot()
